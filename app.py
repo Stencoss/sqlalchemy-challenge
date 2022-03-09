86af1fe0337e5eff0c53bfa3c47ec3a456200335
StevenNimble <stadenoia@gmail.com>
2022-03-08 18:33:37 -0600

    Starting flask

---
+++ app.py
@@ -0,0 +1,57 @@
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+
+#################################################
+# Database Setup
+#################################################
+engine = create_engine("sqlite:///hawaii.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save reference to the table
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+
+#################################################
+# Flask Routes
+#################################################
+
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs</br>"
+        f"/api/v1.0/<start></br>"
+        f"/api/v1.0/<start>/<end>"
+    )
+
+@app.route("/api/v1.0/precipitation")
+def preceipation():
+    return (f"Prep")
+
+@app.route("/api/v1.0/stations")
+def stations():
+    return (f"stations")
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+    return (f"tobs")
